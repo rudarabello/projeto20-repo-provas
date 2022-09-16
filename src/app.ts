58aebb0acf3dfcbd8eba185455bebd1169dca761
ruda <ruda.rabello.silva@gmail.com>
2022-09-16 20:34:56 -0300

    feat: supertest and config

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { json } from 'express';
 import router from './routes/router';
-
+import { errorHandler } from './middlewares/errorHandler';
 dotenv.config();
 
 const app = express();
@@ -10,9 +10,6 @@ const app = express();
 app.use(cors());
 app.use(json());
 app.use(router);
+app.use(errorHandler);
 
-const PORT: number = Number(process.env.PORT) || 5000;
-
-app.listen(PORT, () => {
-    console.log('Server running on port', PORT);
-});
+export default app;
