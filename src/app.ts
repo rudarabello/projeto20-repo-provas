0463633488d84d3e491f7b3651fedbcbcf82b5a0
rudarabello <ruda.rabello.silva@gmail.com>
2022-09-16 13:59:33 -0300

    feat: add test type

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { json } from 'express';
+import express, { json, Router } from 'express';
 
 dotenv.config();
 
@@ -8,8 +8,9 @@ const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(Router);
 
-const PORT: number = Number(process.env.PORT) || 4000;
+const PORT: number = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console
